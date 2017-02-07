05df54108b98632e1e51d1cc2f429f5a667a963b
JamesEarle <j_earle@hotmail.com>
2017-02-07 14:46:01 -0800

    update connection string, delete old IoT Hub

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var deviceAmqp = require('azure-iot-device-amqp');
 var device = require('azure-iot-device');
 
-// Alternate connection string for IoT-Hub-17minds, because SA job inputs can't start with numbers (e.g. 17minds-IoT-Hub)
-// HostName=IoT-Hub-17minds.azure-devices.net;DeviceId=AnotherSampleDevice;SharedAccessKey=x+SoC92iBx42rF80d8vWSNPNJc6kHw4NQC1MEWByqrQ=
-
 // This connection string is custom, will need to change for new devices.
-var client = deviceAmqp.clientFromConnectionString('HostName=17minds-IoT-Hub.azure-devices.net;DeviceId=MySampleDevice;SharedAccessKey=Q/mO1uXTflJdF5PKQZbWHW3MUnbmwHulToXNtXBKyGs=');
+var client = deviceAmqp.clientFromConnectionString('HostName=IoT-Hub-17minds.azure-devices.net;DeviceId=AnotherSampleDevice;SharedAccessKey=x+SoC92iBx42rF80d8vWSNPNJc6kHw4NQC1MEWByqrQ=');
 
 client.open(err => {
     // Handle C2D messages
