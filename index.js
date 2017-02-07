51a14cdb31b1416f0156ff44bdecf50713117ebb
JamesEarle <j_earle@hotmail.com>
2017-02-07 12:30:11 -0800

    added alt connection string for different IoT Hub

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var deviceAmqp = require('azure-iot-device-amqp');
 var device = require('azure-iot-device');
 
+// Alternate connection string for IoT-Hub-17minds, because SA job inputs can't start with numbers (e.g. 17minds-IoT-Hub)
+// HostName=IoT-Hub-17minds.azure-devices.net;DeviceId=AnotherSampleDevice;SharedAccessKey=x+SoC92iBx42rF80d8vWSNPNJc6kHw4NQC1MEWByqrQ=
+
 // This connection string is custom, will need to change for new devices.
 var client = deviceAmqp.clientFromConnectionString('HostName=17minds-IoT-Hub.azure-devices.net;DeviceId=MySampleDevice;SharedAccessKey=Q/mO1uXTflJdF5PKQZbWHW3MUnbmwHulToXNtXBKyGs=');
 
